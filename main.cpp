9cd7cb678664c891036aad1c4473a17218ef46ee
domin <>
2017-04-28 21:07:11 +0200

    insert a comment

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: domin
+ *
+ * Created on 28. April 2017, 21:04
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    //first test
+    cout << "hello world";
+    return 0;
+}
+
